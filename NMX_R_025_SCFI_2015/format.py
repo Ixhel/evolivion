bd02dc2f86a88f5cc40244affad491ddc9df65be
Ix <contacto@ixhel.mx>
2024-05-12 14:39:01 -0600

    commit culero

--- NMX_R_025_SCFI_2015/format.py
+++ NMX_R_025_SCFI_2015/format.py
@@ -1,18 +1,13 @@
 import re #regular expresions
-from django.utils.timezone import datetime
-from django.http import HttpResponse
 
-def format_name (request, name):
-    now = datetime.now()
-    formatted_now = now.strftime("%d %B, %Y a las %X")
-    formatted_name = re.match("[a-zA-Z]+", name)
+def valid_text (text):
     
-    if formatted_name:
-        clean_name = formatted_name.group(0)
+    formatted_text = re.match("[a-zA-Z]+", text)
+    
+    if formatted_text:
+        content = formatted_text.group(0)
     else:
-        clean_name = "usr"
+        content = 'null'
     
-    content = "Usuario: " + clean_name + "Sesión iniciada: " + formatted_now
-    
-    return HttpResponse(content)
+    return content
 
--- NMX_R_025_SCFI_2015/models.py
+++ NMX_R_025_SCFI_2015/models.py
@@ -13,7 +13,38 @@ class LogMessage(models.Model):
 class LogEval(models.Model):
     #Ingesar variables requeridas en la base de datos para calcular resultado
     #Evaluar area de seguridad para despliegue de resultados (Verificar que no se pueda modificar<generar pdf>)
-    #
     
     def __str__(self) -> str:
         return super().__str__()
+    
+class Sector(models.Model):
+    PUBLICO = "PU"
+    PRIVADO = "PR"
+    SOCIAL = "SO"
+    SECTOR_TIPO = {
+        PUBLICO: "Público",
+        PRIVADO: "Privado",
+        SOCIAL:"Social"
+    }
+    sector = models.CharField(max_length=2, choices=SECTOR_TIPO, unique=True, primary_key=True)
+
+class Sector_productivo(models.Model):
+    tamaño = models.CharField(max_length=7)
+    sector_productivo = models.CharField(max_length=21, unique=True)
+    cant_personal = models.CharField(max_length=8, unique=True)
+    
+class Giro(models.Model):
+    giro = models.CharField(max_length=9)
+    actividad = models.CharField(max_length=15)
+
+class Empresa(models.Model):
+    nombre_ct = models.CharField(max_length=300)
+    razon_social = models.CharField(max_length=20)
+    rfc = models.CharField(max_length=50)
+    equipo_seguridad = models.BooleanField
+    equipo_seguridad_descripcion = models.TextField(max_length=300)
+    adicional_logistica = models.TextField(max_length=200)
+    multisitio = models.BooleanField(default=False)
+    id_sector = models.ForeignKey(Sector, on_delete=models.CASCADE)
+    id_sector_productivo = models.ForeignKey(Sector_productivo, on_delete=models.CASCADE)
+    id_giro = models.ForeignKey(Giro, on_delete=models.CASCADE)
